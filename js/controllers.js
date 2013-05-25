53079dcc4d8bb46a55821a3422f2cf9b01ff01f2
Nick Gamberini <nickg@nickg.org>
2013-05-25 02:28:59 -0400

    Responsive design is fun

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,15 @@ function LastFmCtrl($scope, $http, $timeout) {
 	!function tick() {
 		$http({method: 'GET', url: 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=nickelpro&api_key=ffd9e00b1647c0824ce7cb0a0aa44ce1&format=json'}).
 		success(function(data, status, headers, config) {
+			$scope.tracks = [];
+			for(var i=0;i<data.recenttracks.track.length;i++) {
+				var track = data.recenttracks.track[i];
+				if (typeof track.date != 'undefined') {
+					track.time = moment.unix(track.date.uts).fromNow();
+				} else {
+					track.time = 'Now Playing';
+				}
+			}
 			$scope.tracks = data.recenttracks.track;
 			$timeout(tick, 5000);
 		}).
